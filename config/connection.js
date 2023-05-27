56f7d8a4864839b2ead72836a36ee94eb5315c02
Tanmoy5992 <tanmoychakraborty105@gmail.com>
2023-05-28 00:10:11 +0530

    seeders

--- config/connection.js
+++ config/connection.js
@@ -10,14 +10,14 @@ sequelize
     console.error("Unable to connect to the database:", err);
   });
 
-sequelize
-  .sync({ alter: true })
-  .then(() => {
-    console.log("sync done");
-  })
-  .catch((err) => {
-    console.log(err);
-    console.log("sync err");
-  });
+// sequelize
+//   .sync({ alter: true })
+//   .then(() => {
+//     console.log("sync done");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//     console.log("sync err");
+//   });
 
 module.exports = { sequelize };
